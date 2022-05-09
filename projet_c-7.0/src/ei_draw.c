204fc6afd05d35d7ba0e01f0bf560cc747b8fb55
Axel Jeunet--Bailly <axel.jeunet--bailly@grenoble-inp.org>
2022-05-09 21:32:36 +0200

    ok

--- projet_c-7.0/src/ei_draw.c
+++ projet_c-7.0/src/ei_draw.c
@@ -1,3 +1,19 @@
-//
-// Created by jules on 03/05/22.
-//
+#include "hw_interface.h"
+
+uint32_t		ei_map_rgba		(ei_surface_t surface, ei_color_t color)
+{
+    int *ia;
+    int *ib;
+    int *ir;
+    int *ib;
+    hw_surface_get_channel_indices(surface, ir, ig, ib, ia);
+    uint32_t tot = 0;
+    tot += color.red << (8 * (*ir));
+    tot += color.blue << (8 * (*ib));
+    tot += color.green << (8* (*ir));
+    if (*ia == -1) {
+        return tot
+    };
+    tot += color.alpha << (8* (*ia));
+    return tot;
+}
