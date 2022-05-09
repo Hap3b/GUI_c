87a2d5bec389c34099e5478965d06ca3101879fd
Jules Fauchon <Jules.fauchon@grenoble-inp.org>
2022-05-09 13:51:36 +0200

    ajout class frame

---
+++ projet_c-7.0/include/ei_frame_t.h
@@ -0,0 +1,27 @@
+//
+// Created by jules on 06/05/22.
+//
+
+#ifndef PROJETC_IG_EI_FRAME_T_H
+#define PROJETC_IG_EI_FRAME_T_H
+
+#endif //PROJETC_IG_EI_FRAME_T_H*
+#include<ei_widget.h>
+
+typedef struct ei_frame_t {
+        ei_widget_t     widget;
+        ei_relief_t*     relief;
+        char*           title;
+        ei_font_t*       fonte;
+        ei_color_t*      color;
+        ei_anchor_t*     ancre;
+        ei_surface_t*    img;
+        ei_rect_t*       img_rect;
+        ei_anchor_t*     img_anchor;
+
+}ei_frame_t;
+
+extern ei_widgetclass_t classe_frame;
+
+struct ei_frame_t*      ei_frame_allocfunc_t        (void);
+void        ei_frame_releasefunc_t      (struct ei_frame_t*	frame);
