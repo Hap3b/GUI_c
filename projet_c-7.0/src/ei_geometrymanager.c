540fa83322153eab7177676041210d6577e5c6fd
Jules Fauchon <Jules.fauchon@grenoble-inp.org>
2022-05-10 10:35:05 +0200

    Placeur

--- projet_c-7.0/src/ei_geometrymanager.c
+++ projet_c-7.0/src/ei_geometrymanager.c
@@ -3,6 +3,114 @@
 //
 
 #include<ei_widget.h>
+#include<ei_geometrymanager.h>
+extern ei_geometrymanager_t* geometrymanager_cree;
+
+typedef struct ei_geometry_placeur_t {
+        ei_geometrymanager_t* manager;
+        ei_anchor_t* anchor;
+        int*  x;
+        int*  y;
+        float*  rel_x;
+        float*  rel_y;
+        int*			width;
+        int*			height;
+        float*			rel_width;
+        float*			rel_height;
+}ei_geometry_placeur_t;
+
+void	ei_runfunc_placeur_t    (struct ei_widget_t*	widget)
+{
+        if (((ei_geometry_placeur_t*)(widget -> geom_params)) -> anchor =  ei_anc_northwest || ((ei_geometry_placeur_t*)(widget -> geom_params)) -> anchor ==  NULL)
+        {
+                widget -> screen_location.top_left.x = widget->parent->screen_location.top_left.x + ((ei_geometry_placeur_t*)(widget -> geom_params)) -> x;
+                widget -> screen_location.top_left.y = widget->parent->screen_location.top_left.y + ((ei_geometry_placeur_t*)(widget -> geom_params)) -> y;
+                widget -> screen_location.size.width = ((ei_geometry_placeur_t*)(widget -> geom_params)) -> width;
+                widget -> screen_location.size.height = ((ei_geometry_placeur_t*)(widget -> geom_params)) -> height;
+        }
+        else
+        {
+                if(((ei_geometry_placeur_t*)(widget -> geom_params)) -> anchor =  ei_anc_north)
+                {
+                        widget -> screen_location.top_left.x = widget->parent->screen_location.top_left.x + *((ei_geometry_placeur_t*)(widget -> geom_params)) -> x - *(((ei_geometry_placeur_t*)(widget -> geom_params)) -> width)/2;
+                        widget -> screen_location.top_left.y = widget->parent->screen_location.top_left.y + *((ei_geometry_placeur_t*)(widget -> geom_params)) -> y;
+                        widget -> screen_location.size.width = ((ei_geometry_placeur_t*)(widget -> geom_params)) -> width;
+                        widget -> screen_location.size.height = ((ei_geometry_placeur_t*)(widget -> geom_params)) -> height;
+                }
+                else
+                {
+                        if(((ei_geometry_placeur_t*)(widget -> geom_params)) -> anchor =  ei_anc_east)
+                        {
+                                widget -> screen_location.top_left.x = widget->parent->screen_location.top_left.x + *((ei_geometry_placeur_t*)(widget -> geom_params)) -> x - *(((ei_geometry_placeur_t*)(widget -> geom_params)) -> width);
+                                widget -> screen_location.top_left.y = widget->parent->screen_location.top_left.y + *((ei_geometry_placeur_t*)(widget -> geom_params)) -> y - *(((ei_geometry_placeur_t*)(widget -> geom_params)) -> height)/2;
+                                widget -> screen_location.size.width = ((ei_geometry_placeur_t*)(widget -> geom_params)) -> width;
+                                widget -> screen_location.size.height = ((ei_geometry_placeur_t*)(widget -> geom_params)) -> height;
+                        }
+                        else
+                        {
+                                if (((ei_geometry_placeur_t*)(widget -> geom_params)) -> anchor =  ei_anc_southeast)
+                                {
+                                        widget -> screen_location.top_left.x = widget->parent->screen_location.top_left.x + *((ei_geometry_placeur_t*)(widget -> geom_params)) -> x - *(((ei_geometry_placeur_t*)(widget -> geom_params)) -> width);
+                                        widget -> screen_location.top_left.y = widget->parent->screen_location.top_left.y + *((ei_geometry_placeur_t*)(widget -> geom_params)) -> y - *(((ei_geometry_placeur_t*)(widget -> geom_params)) -> height);
+                                        widget -> screen_location.size.width = ((ei_geometry_placeur_t*)(widget -> geom_params)) -> width;
+                                        widget -> screen_location.size.height = ((ei_geometry_placeur_t*)(widget -> geom_params)) -> height;
+                                }
+                                else
+                                {
+                                        if (((ei_geometry_placeur_t*)(widget -> geom_params)) -> anchor =  ei_anc_south)
+                                        {
+                                                widget -> screen_location.top_left.x = widget->parent->screen_location.top_left.x + *((ei_geometry_placeur_t*)(widget -> geom_params)) -> x - *(((ei_geometry_placeur_t*)(widget -> geom_params)) -> width)/2;
+                                                widget -> screen_location.top_left.y = widget->parent->screen_location.top_left.y + *((ei_geometry_placeur_t*)(widget -> geom_params)) -> y - *(((ei_geometry_placeur_t*)(widget -> geom_params)) -> height);
+                                                widget -> screen_location.size.width = ((ei_geometry_placeur_t*)(widget -> geom_params)) -> width;
+                                                widget -> screen_location.size.height = ((ei_geometry_placeur_t*)(widget -> geom_params)) -> height;
+                                        }
+                                        else
+                                        {
+                                                if (((ei_geometry_placeur_t*)(widget -> geom_params)) -> anchor =  ei_anc_southwest)
+                                                {
+                                                        widget -> screen_location.top_left.x = widget->parent->screen_location.top_left.x + *((ei_geometry_placeur_t*)(widget -> geom_params)) -> x;
+                                                        widget -> screen_location.top_left.y = widget->parent->screen_location.top_left.y + *((ei_geometry_placeur_t*)(widget -> geom_params)) -> y - *(((ei_geometry_placeur_t*)(widget -> geom_params)) -> height);
+                                                        widget -> screen_location.size.width = ((ei_geometry_placeur_t*)(widget -> geom_params)) -> width;
+                                                        widget -> screen_location.size.height = ((ei_geometry_placeur_t*)(widget -> geom_params)) -> height;
+                                                }
+                                                else
+                                                {
+                                                        if (((ei_geometry_placeur_t*)(widget -> geom_params)) -> anchor =  ei_anc_west)
+                                                        {
+                                                                widget -> screen_location.top_left.x = widget->parent->screen_location.top_left.x + *((ei_geometry_placeur_t*)(widget -> geom_params)) -> x;
+                                                                widget -> screen_location.top_left.y = widget->parent->screen_location.top_left.y + *((ei_geometry_placeur_t*)(widget -> geom_params)) -> y - *(((ei_geometry_placeur_t*)(widget -> geom_params)) -> height)/2;
+                                                                widget -> screen_location.size.width = ((ei_geometry_placeur_t*)(widget -> geom_params)) -> width;
+                                                                widget -> screen_location.size.height = ((ei_geometry_placeur_t*)(widget -> geom_params)) -> height;
+                                                        }
+                                                        else
+                                                        {
+                                                                if (((ei_geometry_placeur_t*)(widget -> geom_params)) -> anchor =  ei_anc_northeast)
+                                                                {
+                                                                        widget -> screen_location.top_left.x = widget->parent->screen_location.top_left.x + *((ei_geometry_placeur_t*)(widget -> geom_params)) -> x - *(((ei_geometry_placeur_t*)(widget -> geom_params)) -> width);
+                                                                        widget -> screen_location.top_left.y = widget->parent->screen_location.top_left.y + *((ei_geometry_placeur_t*)(widget -> geom_params)) -> y;
+                                                                        widget -> screen_location.size.width = ((ei_geometry_placeur_t*)(widget -> geom_params)) -> width;
+                                                                        widget -> screen_location.size.height = ((ei_geometry_placeur_t*)(widget -> geom_params)) -> height;
+                                                                }
+                                                        }
+                                                }
+                                        }
+                                }
+                        }
+                }
+        }
+};
+void	ei_releasefunc_placeur_t	(struct ei_widget_t*	widget)
+{
+
+}
+
+struct ei_geometrymanager_t geom_placeur_t = {
+        "geom_placeur_t",
+        &ei_runfunc_placeur_t,
+        &ei_releasefunc_placeur_t,
+        NULL
+};
+
 
 void			ei_place			(ei_widget_t*		widget,
                                      ei_anchor_t*		anchor,
@@ -16,4 +124,25 @@ void			ei_place			(ei_widget_t*		widget,
                                      float*			rel_height)
 {
 
+        struct ei_geometry_placeur_t* placeur_widget;
+        placeur_widget -> manager = &geom_placeur_t;
+        placeur_widget -> anchor = anchor;
+        placeur_widget ->x = x;
+        placeur_widget -> y = y;
+        placeur_widget -> width = width;
+        placeur_widget -> height = height;
+        placeur_widget -> rel_x = rel_x;
+        placeur_widget -> rel_y = rel_y;
+        placeur_widget -> rel_width = rel_width;
+        placeur_widget -> rel_height = rel_height;
+        widget -> geom_params = placeur_widget;
+        ei_runfunc_placeur_t(widget);
+
+
+}
+
+void			ei_geometrymanager_register	(ei_geometrymanager_t* geometrymanager)
+{
+        geometrymanager->next = geometrymanager_cree;
+        geometrymanager_cree = geometrymanager;
 }
